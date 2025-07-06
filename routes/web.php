ce0c9ba02bc0b53c399d4b0c8d3e00556618940d
tiarawr <nailaym@students.unnes.ac.id>
2025-07-06 16:41:32 +0700

    Add debug route to check Railway configuration

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
+// Debug route for Railway deployment
+Route::get('/debug-config', function () {
+    return response()->json([
+        'app_env' => config('app.env'),
+        'app_debug' => config('app.debug'),
+        'database_connection' => config('database.default'),
+        'midtrans_merchant_id' => config('midtrans.merchant_id') ? 'SET' : 'NOT SET',
+        'midtrans_client_key' => config('midtrans.client_key') ? 'SET' : 'NOT SET',
+        'midtrans_server_key' => config('midtrans.server_key') ? 'SET' : 'NOT SET',
+        'midtrans_is_production' => config('midtrans.is_production'),
+        'cache_store' => config('cache.default'),
+        'session_driver' => config('session.driver'),
+    ]);
+});
+
 Route::get('/', function () {
     $apartments = \App\Models\Apartment::with('reviews')->get();
     
